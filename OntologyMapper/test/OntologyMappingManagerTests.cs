9f0455f04897891533f0ed517daf872973146cd4
Joe Beernink <jobee@microsoft.com>
2022-08-16 13:53:09 -0700

    Split the validation

--- OntologyMapper/test/OntologyMappingManagerTests.cs
+++ OntologyMapper/test/OntologyMappingManagerTests.cs
@@ -208,80 +208,6 @@ namespace Microsoft.SmartFacilities.OntologyMapper.Test
             Assert.Null(propertyFill);
         }
 
-        [Theory]
-        [InlineData("Mappings.Mapped.Json.v0.BrickRec.mapped_json_v0_dtdlv2_Brick_1_3-REC_4_0.json")]
-        [InlineData("Mappings.Mapped.Json.v0.BrickRec.mapped_json_v0_dtdlv3_Brick_1_3-REC_4_0.json")]
-        public void ValidateEmbeddedResourceDtmisAreValidFormat(string resourcePath)
-        {
-            var mockLogger = new Mock<ILogger>();
-            var resourceLoader = new EmbeddedResourceOntologyMappingLoader(mockLogger.Object, resourcePath);
-            var ontologyMappingManager = new OntologyMappingManager(resourceLoader);
-
-            var exceptions = new List<string>();
-            foreach (var mapping in ontologyMappingManager.OntologyMapping.InterfaceRemaps)
-            {
-                try
-                {
-                    var inputDtmi = new Dtmi(mapping.InputDtmi);
-                }
-                catch (ParsingException)
-                {
-                    exceptions.Add($"Invalid input DTMI: {mapping.InputDtmi}");
-                }
-
-                try
-                {
-                    var outputDtmi = new Dtmi(mapping.OutputDtmi);
-                }
-                catch (ParsingException)
-                {
-                    exceptions.Add($"Invalid output DTMI: {mapping.OutputDtmi}");
-                }
-            }
-
-            // Verify that the Interface Remaps are unique for an input interface
-            foreach (var interfaceRemap in ontologyMappingManager.OntologyMapping.InterfaceRemaps)
-            {
-                var matchingRemapsCount = ontologyMappingManager.OntologyMapping.InterfaceRemaps.Count(p => p.InputDtmi == interfaceRemap.InputDtmi);
-                if (matchingRemapsCount > 1)
-                {
-                    exceptions.Add($"Duplicate InterfaceRemap: {interfaceRemap.InputDtmi}");
-                }
-            }
-
-            // Verify that the Interface Remaps are unique for an input interface
-            foreach (var relationshipRemap in ontologyMappingManager.OntologyMapping.RelationshipRemaps)
-            {
-                var matchingRemapsCount = ontologyMappingManager.OntologyMapping.RelationshipRemaps.Count(p => p.InputRelationship == relationshipRemap.InputRelationship);
-                if (matchingRemapsCount > 1)
-                {
-                    exceptions.Add($"Duplicate RelationshipRemap: {relationshipRemap.InputRelationship}");
-                }
-            }
-
-            // Verify that the property projections are unique for an output property
-            foreach (var projection in ontologyMappingManager.OntologyMapping.PropertyProjections)
-            {
-                var matchingProjectionsCount = ontologyMappingManager.OntologyMapping.PropertyProjections.Count(p => p.OutputPropertyName == projection.OutputPropertyName);
-                if (matchingProjectionsCount > 1)
-                {
-                    exceptions.Add($"Duplicate PropertyProjection: {projection.OutputPropertyName}");
-                }
-            }
-
-            // Verify that the fill properties are unique for an output property
-            foreach (var fillProperty in ontologyMappingManager.OntologyMapping.FillProperties)
-            {
-                var matchingFillPropertyCount = ontologyMappingManager.OntologyMapping.FillProperties.Count(p => p.OutputPropertyName == fillProperty.OutputPropertyName);
-                if (matchingFillPropertyCount > 1)
-                {
-                    exceptions.Add($"Duplicate FillProperty: {fillProperty.OutputPropertyName}");
-                }
-            }
-
-            Assert.Empty(exceptions);
-        }
-
         [Fact]
         public void ValidateTargetOntologyMapping_ReturnsTrue_ForValidOntologyMapping()
         {
@@ -375,14 +301,14 @@ namespace Microsoft.SmartFacilities.OntologyMapper.Test
             return ontologyMapping;
         }
 
-        private IReadOnlyDictionary<Dtmi, DTEntityInfo> GetTargetObjectModel()
+        private static IReadOnlyDictionary<Dtmi, DTEntityInfo> GetTargetObjectModel()
         {
             var objectModelParser = new ModelParser();
             var jsonTexts = LoadDtdl("Space.json");
             return objectModelParser.Parse(jsonTexts);
         }
 
-        private List<string> LoadDtdl(string fileName)
+        private static List<string> LoadDtdl(string fileName)
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resources = assembly.GetManifestResourceNames();
@@ -393,12 +319,10 @@ namespace Microsoft.SmartFacilities.OntologyMapper.Test
             {
                 if (stream != null)
                 {
-                    using (StreamReader reader = new StreamReader(stream))
-                    {
-                        string? result = reader.ReadToEnd();
+                    using StreamReader reader = new(stream);
+                    string? result = reader.ReadToEnd();
 
-                        jsonTexts.Add(result);
-                    }
+                    jsonTexts.Add(result);
                 }
             }
 
