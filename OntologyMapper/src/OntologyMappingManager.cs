f1ce09b40ecca276905fc6c69ea8510d4ee1bd54
Joe Beernink <jobee@microsoft.com>
2022-09-15 11:24:15 -0700

    Updated copyrights

--- OntologyMapper/src/OntologyMappingManager.cs
+++ OntologyMapper/src/OntologyMappingManager.cs
@@ -1,8 +1,5 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="OntologyMappingManager.cs" company="Microsoft">
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 namespace Microsoft.SmartPlaces.Facilities.OntologyMapper
 {
--- OntologyMapper/test/OntologyMappingManagerTests.cs
+++ OntologyMapper/test/OntologyMappingManagerTests.cs
@@ -1,8 +1,5 @@
-// -----------------------------------------------------------------------
-// <copyright file="OntologyMappingTests.cs" company="Microsoft">
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 namespace OntologyMapper.Test
 {
