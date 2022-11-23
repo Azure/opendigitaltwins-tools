28cd191a2ad8c8948d4cc76384d4308481b9b49a
Karl Hammar <karlhammar@microsoft.com>
2022-11-23 14:36:28 +0100

    Stylecop-based docs for IngestionManager.Mapped

--- SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/Extensions/ServiceCollectionExtensions.cs
+++ SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/Extensions/ServiceCollectionExtensions.cs
@@ -11,8 +11,17 @@ namespace Microsoft.SmartPlaces.Facilities.IngestionManager.Mapped.Extensions
     using Microsoft.SmartPlaces.Facilities.IngestionManager.Interfaces;
     using Microsoft.SmartPlaces.Facilities.IngestionManager.Mapped;
 
+    /// <summary>
+    /// Static extension method class for adding an ingestion manager onto a <see cref="IServiceCollection"/>.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds a Mapped ingestion manager to an IServiceCollection, for DI-based deployment.
+        /// </summary>
+        /// <param name="services">Collection of service descriptors to which Mapped Ingestion Manager will be added.</param>
+        /// <param name="options">Mapped ingestion manager options.</param>
+        /// <returns>Collection of service descriptors to which Mapped Ingestion Manager has been added.</returns>
         public static IServiceCollection AddMappedIngestionManager(this IServiceCollection services, Action<MappedIngestionManagerOptions> options)
         {
             services.AddOptions<MappedIngestionManagerOptions>()
--- SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/MappedIngestionManagerOptions.cs
+++ SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/MappedIngestionManagerOptions.cs
@@ -10,18 +10,18 @@ namespace Microsoft.SmartPlaces.Facilities.IngestionManager.Mapped
     using Microsoft.SmartPlaces.Facilities.IngestionManager.Interfaces;
 
     /// <summary>
-    /// Configuration settings needed for connecting to an instance of the Mapped Graph API
+    /// Configuration settings needed for connecting to an instance of the Mapped Graph API.
     /// </summary>
     public class MappedIngestionManagerOptions : IngestionManagerOptions, IInputGraphManagerOptions
     {
         /// <summary>
-        /// Gets or sets an access token used for authorizing against the Mapped Graph API
+        /// Gets or sets an access token used for authorizing against the Mapped Graph API.
         /// </summary>
         [Required]
         public string MappedToken { get; set; } = string.Empty;
 
         /// <summary>
-        /// Gets or sets the value of the Uri used for connecting to the Mapped Graph API
+        /// Gets or sets the value of the Uri used for connecting to the Mapped Graph API.
         /// </summary>
         [Required]
         public string MappedRootUrl { get; set; } = string.Empty;
