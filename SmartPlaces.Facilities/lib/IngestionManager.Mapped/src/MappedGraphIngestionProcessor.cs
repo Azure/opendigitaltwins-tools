a846ae1b1bc41d4fd2da9f78de875fa708387f66
Joe Beernink <jbeernink@willowinc.com>
2023-06-16 13:29:49 -0700

    Converted to warning

--- SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/MappedGraphIngestionProcessor.cs
+++ SmartPlaces.Facilities/lib/IngestionManager.Mapped/src/MappedGraphIngestionProcessor.cs
@@ -160,7 +160,7 @@ namespace Microsoft.SmartPlaces.Facilities.IngestionManager.Mapped
         {
             if (!targetElement.TryGetProperty("id", out var basicDtIdProp))
             {
-                Logger.LogInformation("Building id is missing... skipping.");
+                Logger.LogWarning("Building id is missing... skipping.");
                 return;
             }
 
